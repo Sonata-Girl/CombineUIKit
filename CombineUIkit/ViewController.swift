b3bbc958e5b969679a6925bacde52e7665dc3cf1
SonataGirl <Ino-sonata1988>
2024-05-23 01:37:12 +0300

    Реализован список строк с добавлением

--- CombineUIkit/ViewController.swift
+++ CombineUIkit/ViewController.swift
@@ -10,93 +10,155 @@ import Combine
 
 class ViewController: UIViewController {
 
-    private var dataLabel: UILabel = {
-        let label = UILabel()
-        label.text = "data"
-        label.font = UIFont.systemFont(ofSize: 16, weight: .black)
-        label.textColor = .label
-        return label
+    private var stringTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = UIFont.systemFont(ofSize: 16, weight: .black)
+        textField.textColor = .label
+        textField.placeholder = "Введите строку"
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        return textField
     }()
 
-    private var statusLabel: UILabel = {
-        let label = UILabel()
-        label.text = "status"
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textColor = .label
-        return label
-    }()
+    let addButton = UIButton()
+    let clearButton = UIButton()
+    let tableView = UITableView()
 
-    let buttonCancel = UIButton()
-    let buttonRefresh = UIButton()
-
-    var viewModel = FirstCancelPipelineViewModel()
+    var viewModel = EmptyFailurePublishersViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        buttonCancel.frame = CGRect(x: 50, y: 100, width: 150, height: 50)
-        buttonCancel.setTitle("Отменить заказ", for: .normal)
-        buttonCancel.backgroundColor = .green
-        buttonCancel.addTarget(self, action: #selector(cancel), for: .touchUpInside)
-       
-        buttonRefresh.frame = CGRect(x: 50, y: 200, width: 150, height: 50)
-        buttonRefresh.setTitle("Вызвать такси", for: .normal)
-        buttonRefresh.backgroundColor = .blue
-        buttonRefresh.addTarget(self, action: #selector(refresh), for: .touchUpInside)
+        stringTextField.frame = CGRect(x: 20, y: 100, width: 250, height: 50)
 
-        dataLabel.frame = CGRect(x: 50, y: 300, width: 300, height: 50)
-        statusLabel.frame = CGRect(x: 50, y: 400, width: 300, height: 50)
+        addButton.frame = CGRect(x: 50, y: 200, width: 150, height: 50)
+        addButton.setTitle("Добавить", for: .normal)
+        addButton.addTarget(self, action: #selector(addText), for: .touchUpInside)
+        addButton.setTitleColor(.blue, for: .normal)
 
-        view.addSubview(buttonCancel)
-        view.addSubview(buttonRefresh)
-        view.addSubview(dataLabel)
-        view.addSubview(statusLabel)
+        clearButton.frame = CGRect(x: 200, y: 200, width: 100, height: 50)
+        clearButton.setTitle("Очистить список", for: .normal)
+        clearButton.addTarget(self, action: #selector(clearList), for: .touchUpInside)
+        clearButton.setTitleColor(.blue, for: .normal)
 
+        tableView.frame = CGRect(x: 50, y: 300, width: 300, height: 300)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.layer.borderColor = UIColor.gray.cgColor
+        tableView.layer.borderWidth = 1
+        tableView.layer.cornerRadius = 20
 
-        let subscribe1 = Subscribers.Assign(object: dataLabel, keyPath: \.text)
-        viewModel.$data.subscribe(subscribe1)
-       
-        let subscribe2 = Subscribers.Assign(object: statusLabel, keyPath: \.text)
-        viewModel.$status.subscribe(subscribe2)
+        view.addSubview(stringTextField)
+        view.addSubview(addButton)
+        view.addSubview(clearButton)
+        view.addSubview(tableView)
 
+        bind()
     }
 
-    @objc private func cancel() {
-        viewModel.cancel()
+    @objc private func addText() {
+        viewModel.selectionForAddWord.value = stringTextField.text ?? ""
+        bind()
     }
 
-    @objc private func refresh() {
-        viewModel.refresh()
+    @objc private func clearList() {
+        viewModel.clearList()
+    }
+
+    private func bind() {
+        viewModel.$dataToView
+            .sink(receiveValue: tableView.items { tableView, indexPath, item in
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+                cell.textLabel?.text = item
+                return cell
+            })
+            .store(in: &viewModel.cancellable)
     }
 }
 
-final class FirstCancelPipelineViewModel: ObservableObject {
-    @Published var data: String? = ""
-    @Published var status: String? = ""
-    @Published var validation = ""
+extension UITableView {
+    func items<Element>(_ builder: @escaping (UITableView, IndexPath, Element) -> UITableViewCell) -> ([Element]) -> Void {
+        let dataSource = CombineTableViewDataSource(builder: builder)
+        return { items in
+            dataSource.pushElements(items, to: self)
+        }
+    }
+}
 
-    private var cancellable: AnyCancellable?
+class CombineTableViewDataSource<Element>: NSObject, UITableViewDataSource {
+
+    let build: (UITableView, IndexPath, Element) -> UITableViewCell
+    var elements: [Element] = []
+
+    init(builder: @escaping (UITableView, IndexPath, Element) -> UITableViewCell) {
+        self.build = builder
+        super.init()
+    }
+
+    func pushElements(_ elements: [Element], to tableView: UITableView) {
+        tableView.dataSource = self
+        self.elements = elements
+        tableView.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        elements.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        build(tableView, indexPath, elements[indexPath.row])
+    }
+}
+
+final class EmptyFailurePublishersViewModel: ObservableObject {
+    @Published var dataToView: [String] = []
+    @Published var isButtonVisible = false
+    var selectionForAddWord = CurrentValueSubject<String, Never>("")
+    var inputText = CurrentValueSubject<String, Never>("")
+
+    var datas: [String?] = []
+    var cancellable: Set<AnyCancellable> = []
 
     init() {
-        cancellable = $data
-            .map { [unowned self] value -> String in
-                self.status = "Ищем машину..."
-                return value ?? ""
+        inputText
+            .map { newValue -> Bool in
+                newValue.isEmpty ? false : true
             }
-            .delay(for: 7, scheduler: DispatchQueue.main)
             .sink { [unowned self] value in
-                self.data = "Водитель будет через 10 минут."
-                self.status = "Машина найдена."
+                self.isButtonVisible = value
+            }
+            .store(in: &cancellable)
+        selectionForAddWord
+            .filter {
+                !$0.isEmpty
+            }
+            .sink { [unowned self] newValue in
+                datas.append(newValue)
+                dataToView.removeAll()
+                fetch()
+                inputText.value = ""
+            }
+            .store(in: &cancellable)
+    }
+
+    func fetch() {
+        _ = datas.publisher
+            .flatMap { item -> AnyPublisher<String, Never> in
+                if let item = item {
+                    return Just(item)
+                        .eraseToAnyPublisher()
+                } else {
+                    return Empty(completeImmediately: true)
+                        .eraseToAnyPublisher()
+                }
+            }
+            .sink { [unowned self] item in
+                dataToView.append(item)
             }
     }
 
-    func refresh() {
-        data = "Перезапрос данных"
-    }
-
-    func cancel() {
-        status = "Заказ отменен"
-        cancellable?.cancel()
-        cancellable = nil
+    func clearList() {
+        datas.removeAll()
+        dataToView.removeAll()
+        objectWillChange.send()
     }
 }
