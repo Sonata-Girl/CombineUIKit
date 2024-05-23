0ce08068b259d83bac3d3df4126dae23a5c76fea
SonataGirl <Ino-sonata1988>
2024-05-23 20:02:28 +0300

    Завершена задача проверки простого числа UIkit

--- CombineUIkit/ViewController.swift
+++ CombineUIkit/ViewController.swift
@@ -12,72 +12,108 @@ final class ViewController: UIViewController {
 
     let resultLabel = UILabel()
     let fetchButton = UIButton()
+    let inputNumberField = UITextField()
 
-    var viewModel = FuturePublisherViewModel()
+    var viewModel = FuturePublisherTask8ViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        resultLabel.frame = CGRect(x: 150, y: 200, width: 70, height: 50)
+        inputNumberField.frame = CGRect(x: 20, y: 400, width: 350, height: 30)
+        inputNumberField.layer.borderColor = UIColor.gray.cgColor
+        inputNumberField.layer.borderWidth = 1
+        inputNumberField.layer.cornerRadius = 10
+        inputNumberField.addTarget(self, action: #selector(textFieldValueChanged(_ :)), for: .editingChanged)
+
+        resultLabel.frame = CGRect(x: 80, y: 520, width: 250, height: 30)
         resultLabel.textColor = .green
 
-        fetchButton.frame = CGRect(x: 150, y: 380, width: 70, height: 50)
-        fetchButton.setTitle("Fetch", for: .normal)
+        fetchButton.frame = CGRect(x: 80, y: 450, width: 250, height: 50)
+        fetchButton.setTitle("Проверить простоту числа", for: .normal)
         fetchButton.addTarget(self, action: #selector(fetchAction), for: .touchUpInside)
         fetchButton.setTitleColor(.blue, for: .normal)
 
+        view.addSubview(inputNumberField)
         view.addSubview(resultLabel)
         view.addSubview(fetchButton)
 
         bind()
     }
 
+    @objc private func textFieldValueChanged(_ sender: UITextField) {
+        viewModel.inputText.value = inputNumberField.text ?? ""
+    }
+
     private func bind() {
-        viewModel.$firstResult.subscribe(Subscribers.Assign(object: resultLabel, keyPath: \.text))
+        viewModel.$result.subscribe(Subscribers.Assign(object: resultLabel, keyPath: \.text))
     }
 
     @objc func fetchAction() {
-        viewModel.fetch()
+        viewModel.checkNumber()
     }
 }
 
-final class FuturePublisherViewModel: ObservableObject {
+final class FuturePublisherTask8ViewModel: ObservableObject {
     @Published var firstResult: String? = ""
+    @Published var result: String? = ""
     var cancellable: AnyCancellable?
+    var inputText = CurrentValueSubject<String?, Never>("")
 
-    let futurePublisher = Deferred {
-        Future<String, Never> { promise in
-            promise(.success("FuturePublisher сработал"))
-            print("FuturePublisher сработал")
-        }
-    }
-
-    init() {
-
-    }
-
-    func createFetch(url: URL) -> AnyPublisher<String?, Error> {
-        Future { promise in
-            let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                if let error = error {
-                    promise(.failure(error))
-                    return
+    func checkNumber() {
+        cancellable = Future<String, Never> { promise in
+            for number in 2...500 {
+                if let intNumber = Int(self.inputText.value ?? ""), intNumber != Int(number),
+                   intNumber % Int(number) == 0 {
+                    print(number)
+                    promise(.success("\(self.inputText.value ?? "") - Это не простое число."))
                 }
-                promise(.success(response?.url?.absoluteString ?? ""))
             }
-            task.resume()
+            promise(.success("\(self.inputText.value ?? "") - Это простое число."))
         }
-        .eraseToAnyPublisher()
-    }
-
-    func fetch() {
-        guard let url = URL(string: "https://google.com") else { return }
-        cancellable = createFetch(url: url)
-            .receive(on: RunLoop.main)
-            .sink { completion in
-                print(completion)
-            } receiveValue: { [unowned self] value in
-                firstResult = value ?? ""
-            }
+        .sink { [unowned self] value in
+           self.result = String(value)
+        }
     }
 }
+
+
+//final class FuturePublisherViewModel: ObservableObject {
+//    @Published var firstResult: String? = ""
+//    var cancellable: AnyCancellable?
+//
+//    let futurePublisher = Deferred {
+//        Future<String, Never> { promise in
+//            promise(.success("FuturePublisher сработал"))
+//            print("FuturePublisher сработал")
+//        }
+//    }
+//
+//    init() {
+//
+//    }
+//
+//    func createFetch(url: URL) -> AnyPublisher<String?, Error> {
+//        Future { promise in
+//            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+//                if let error = error {
+//                    promise(.failure(error))
+//                    return
+//                }
+//                promise(.success(response?.url?.absoluteString ?? ""))
+//            }
+//            task.resume()
+//        }
+//        .eraseToAnyPublisher()
+//    }
+//
+//    func fetch() {
+//        guard let url = URL(string: "https://google.com") else { return }
+//        cancellable = createFetch(url: url)
+//            .receive(on: RunLoop.main)
+//            .sink { completion in
+//                print(completion)
+//            } receiveValue: { [unowned self] value in
+//                firstResult = value ?? ""
+//            }
+//    }
+//}
